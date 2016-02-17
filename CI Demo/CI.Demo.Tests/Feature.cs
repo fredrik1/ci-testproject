efcd15d1db61931328c2547079e3721a23da7673
Fredrik Samuelsson <fredrik319@gmail.com>
2016-02-17 10:21:55 +0100

    Set up projects

---
+++ CI Demo/CI.Demo.Tests/Feature.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CI.Demo.Tests
+{
+    public class Feature
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void ValidatesNameIsNotNull()
+            {
+                Assert.Throws(() =>
+                {
+                    new Demo.Feature(null);
+                });
+            }
+
+            [Fact]
+            public void ValidatesNameIsNotEmpty()
+            {
+                Assert.Throws(() =>
+                {
+                    new Demo.Feature("");
+                });
+            }
+
+            [Fact]
+            public void StoresTheNameInLowercase()
+            {
+                var subject = new Demo.Feature("My UPPERcase Feature");
+                subject.Name.Should().Equal("my uppercase feature");
+            }
+        }
+    }
+}
